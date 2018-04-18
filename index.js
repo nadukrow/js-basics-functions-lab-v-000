5cca595a97d36a54c3411ebd59b9cea741445cd8
Nana Adu-Krow <nadukrow@gmail.com>
2018-04-18 23:38:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,3 +19,18 @@ function distanceTravelledInFeet (startlocale, endlocale) {
   return Math.abs(startlocale - endlocale) * 264;
   
 }
+
+function calculatesFarePrice (startlocale, endlocale) {
+  const dist = distanceTravelledInFeet (start, destination);
+
+  switch (true) {
+    case dist <= 400:
+      return 0;
+    case dist <= 2000:
+      return (dist - 400) * 0.02;
+    case dist <= 2500:
+      return 25;
+    default:
+      return 'cannot travel that far';
+  }
+}
