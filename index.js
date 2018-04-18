8f5f9e575c4ab4e4d016deb37a4ec4f6d0e9af49
Nana Adu-Krow <nadukrow@gmail.com>
2018-04-18 23:35:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 // Code your solution in this file!
 
-// function distanceFromHQInBlocks(distance) {
-//   return Math.abs(distance - 42);
-// }
-
-function distanceFromHqInBlocks (st) {
-  return Math.abs(st - 42);
+function distanceFromHQInBlocks(distance) {
+  return Math.abs(distance - 42);
 }
-
 
 // function distanceFromHQInFeet() {
   
